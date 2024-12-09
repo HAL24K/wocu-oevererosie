8fba56472946eb6ced2ea8700978e88c4be13d73
ondrej-maince <ondrej.urban@maince.ai>
2024-12-09 11:58:57 +0100

    codebase setup (#1)
    some initial files

---
+++ src/constants.py
@@ -0,0 +1,8 @@
+"""All the hardcoded values live here. This is the place to change them if needed."""
+
+# where the Netherlands is
+CENTRE_NL_LON = 5.2913
+CENTRE_NL_LAT = 52.1326
+
+DEFAULT_NL_ZOOM = 8  # at this zoom most of the country is visible when centred of the centre of the country
+EPSG_WGS84 = 4326
